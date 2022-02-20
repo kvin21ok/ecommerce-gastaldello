1f9025a83d33f57c353ddefb6065b3108597db4b
kevin Gasta <kevin.gastaldello21@gmail.com>
2022-02-19 23:19:56 -0300

    creacion de la navbar y aplicado de estilo en app.css

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+const Navigation =  ()=>{
+    return(
+        <div>
+            
+            <ul>
+            ecommerce             
+                <a href ="#">inicio</a>
+                <a href ="#">ofertas</a>
+                <a href ="#">catalogos</a>
+                <a href ="#">contactanos</a>
+            </ul>
+        </div>
+    )
+}
+export default Navigation
