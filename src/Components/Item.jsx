298296c4617711cbde4225e2d3ef9f54e2a74e24
kevin Gasta <kevin.gastaldello21@gmail.com>
2022-03-03 20:35:06 -0300

    configuracion del mensaje cargando... mientras carga lista de items

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -6,6 +6,7 @@ import ItemCount from "./ItemCount";
 
 const Item = ()=>{
     const [productos, setProductos] = useState([])
+    const [loading, setLoading] = useState (true)
 
     useEffect(()=>{
         getFetch
@@ -14,13 +15,16 @@ const Item = ()=>{
         })
         .then((res) => setProductos(res))
         .catch(err => console.log(err))
+        .finally(()=> setLoading(false))
 
     },[])
     console.log(productos)
 
     return(
         <div>
-            {Products.map((prod)=> <li key={prod.id} className = "item"> {prod.name} {prod.price} {prod.pictureUrl} <ItemCount/> </li>)}
+            { loading ? <h1>Cargando...</h1> 
+            :
+            Products.map((prod)=> <li key={prod.id} className = "item"> {prod.name} {prod.price} {prod.pictureUrl} <ItemCount/> </li>)}
         </div>
     )
 }
