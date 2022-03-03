32782b1cc2942d5116d4c075a93d6d76faac1865
kevin Gasta <kevin.gastaldello21@gmail.com>
2022-03-02 21:39:45 -0300

    creacion item.jsx y itemlist.jsx

--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ItemCount from "./ItemCount";
+import ItemList from "./ItemList";
 
 const ItemListContainer = ({ greetings })=>{
     return(
         <div>
             <h3>{ greetings }</h3>
             <ItemCount/>
+            <ItemList/>
         </div>
     )
 };
